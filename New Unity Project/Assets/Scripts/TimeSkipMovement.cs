9502d4ccb0793d7a7ca2bf65dd1f475628b6b19b
Nerdikon57 <30872739+Nerdikon57@users.noreply.github.com>
2021-01-30 15:04:46 -0600

    Physics engine made. Character can move, jump, and time travel. Currently, all sprites are placeholders and will be replaced once we have completed our own art.

---
+++ New Unity Project/Assets/Scripts/TimeSkipMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSkipMovement : MonoBehaviour
+{
+    int timeSkipCount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSkipCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //When Q is pressed, this teleports whatever object this script is attatched to up or down by 20 units depending on how many times it has been pressed so far.
+        //This creates the effect of moving from present to future. This script is applied to the player and the main camera. 
+        if (Input.GetKeyDown(KeyCode.Q) && timeSkipCount < 5)
+        {
+            timeSkipCount++;
+            if (timeSkipCount % 2 == 0)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y - 20, transform.position.z);
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + 20, transform.position.z);
+            }
+
+
+        }
+    }
+}
